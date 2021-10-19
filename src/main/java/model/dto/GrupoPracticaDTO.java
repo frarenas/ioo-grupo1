6b1171b04a66dd6bb1483ebc31c13f4a39f2e6da
Francisco Arenas <francisco.arenas@mercadolibre.com>
2021-10-19 20:48:17 -0300

    dtos

--- src/main/java/model/dto/GrupoPracticaDTO.java
+++ src/main/java/model/dto/GrupoPracticaDTO.java
@@ -1,7 +1,13 @@
 package model.dto;
 
+import model.GrupoPractica;
+
 public class GrupoPracticaDTO {
     private String nombre;
+
+    public GrupoPracticaDTO(String nombre) {
+        this.nombre = nombre;
+    }
 
     public String getNombre() {
         return nombre;
@@ -10,4 +16,9 @@ public class GrupoPracticaDTO {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public static GrupoPracticaDTO fromEntity(GrupoPractica grupoPractica) {
+        return new GrupoPracticaDTO(grupoPractica.getNombre());
+    }
+
 }
--- src/main/java/model/dto/ResultadoPeticionDTO.java
+++ src/main/java/model/dto/ResultadoPeticionDTO.java
@@ -1,7 +1,14 @@
 package model.dto;
 
+import model.Peticion;
+import model.ResultadoPeticion;
+
 public class ResultadoPeticionDTO {
     private Long resultado;
+
+    public ResultadoPeticionDTO(Long resultado) {
+        this.resultado = resultado;
+    }
 
     public Long getResultado() {
         return resultado;
@@ -9,5 +16,11 @@ public class ResultadoPeticionDTO {
 
     public void setResultado(Long resultado) {
         this.resultado = resultado;
+    }
+
+    public static ResultadoPeticionDTO fromEntity(ResultadoPeticion resultadoPeticion) {
+        return new ResultadoPeticionDTO(
+                resultadoPeticion.getResultado()
+        );
     }
 }
